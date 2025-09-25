90075897589505131116ea11bbd8fa494fdacc19
EliBunnell <bunnelec@mail.uc.edu>
2025-09-25 15:05:32 -0400

    Done

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
 
+
         int birthMonth = 9;
         String trash = "";
         Scanner input = new Scanner(System.in);
